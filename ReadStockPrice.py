4a05bf62ae58f28a241bb7ed7e5feb5fed43b652
pjayaraj1989 <33594359+pjayaraj1989@users.noreply.github.com>
2022-10-06 12:37:49 +0530

    Reading investment information from a json
    --Updated with reading investment information from a json
    --Needs MyStockData.json file in the same dir with total units, and symbols owned

--- ReadStockPrice.py
+++ ReadStockPrice.py
@@ -1,18 +1,22 @@
+import json
 from unicodedata import name
 import yfinance as yf
 from forex_python.converter import CurrencyRates
 import sys
 
+# exit with error msg
 def Error_Exit(msg):
     input(msg)
     exit(1)
 
+# use forex_python module to convert currency
 def ConvertCurrency(amount, from_curr, to_curr):
     final_amt=0.0
     cr = CurrencyRates()
     final_amt = cr.convert(from_curr, to_curr, amount)
     return round(final_amt, 2)
 
+# get stp for a symbol, return a tuple of price,curr
 def GetStockPrice(stockname):
     data=yf.Ticker(stockname)
     if not 'symbol' in data.info.keys():
@@ -21,14 +25,28 @@ def GetStockPrice(stockname):
     price = data.info['open']
     return price, currency
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        Error_Exit ('Error! Give the symbol as an arg')
-    stockname=sys.argv[1]
-    if stockname is None or '':
-        Error_Exit ('Error! Enter valid symbol!')
-
-    # a tuple
-    stock_price = GetStockPrice(stockname)
+# get total stock value
+def GetStockValue(symbol, units):
+    stock_price = GetStockPrice(symbol)
     stock_price_inr = ConvertCurrency(stock_price[0], stock_price[1], 'INR')
-    print (str(stock_price_inr) + ' INR')
+    return round(stock_price_inr * units, 2)
+
+if __name__ == "__main__":
+    json_file = 'MyStockData.json'
+    import pandas as pd
+    with open(json_file) as data_file:
+        data = json.load(data_file)
+
+    if data is None or {}:
+        Error_Exit('Invalid user data')
+
+    total_investment_stocks = []
+    for stock_list in data.keys():
+        for stock in data[stock_list]:
+            total_investment_stocks.append(stock)
+
+    for stock_info in total_investment_stocks:
+        for symbol, units in stock_info.items():
+            print ('Stock: %s, Units: %s, Total Value(INR): %s' %(symbol, units, GetStockValue(symbol, units)))
+        
+    
